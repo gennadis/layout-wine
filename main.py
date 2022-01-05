bbd664537073990502a58036d4dc5204a70cee44
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-05 19:14:50 +0300

    Add Jinja2 templating

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from datetime import date
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+env = Environment(
+    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+)
+
+template = env.get_template("template.html")
+
+
+winery_age = 101
+suffix = "год"
+
+rendered_page = template.render(
+    winery_age=winery_age,
+    suffix=suffix,
+)
+
+with open("index.html", "w", encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
