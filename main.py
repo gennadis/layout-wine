2d2e4bf2bf07ad0db49c0181e9f4e9f4dff879e5
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-06 12:18:28 +0300

    Update rendering drink cards from excel file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import date
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+
 
 from pandas import read_excel
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -27,24 +29,31 @@ def get_suffix(age: int) -> str:
     return "лет"
 
 
-def get_wines(filename: str) -> list[dict]:
+def get_drinks(filename: str) -> dict:
     """Get wines from excel file."""
-    wines_df = read_excel(filename)
-    return wines_df.to_dict(orient="records")
+    df = read_excel(filename, na_values=None, keep_default_na=False)
+    beverages = df.to_dict(orient="records")
+    products = defaultdict(list)
+
+    for beverage in beverages:
+        products[beverage["Категория"]].append(beverage)
+
+    return products
 
 
 winery_age: int = get_age(1920)
 suffix: str = get_suffix(winery_age)
-wines: list[dict] = get_wines("wine.xlsx")
+drinks: dict = get_drinks("wine2.xlsx")
 
 rendered_page = template.render(
     winery_age=winery_age,
     suffix=suffix,
-    wines=wines,
+    drinks=drinks,
 )
 
 with open("index.html", "w", encoding="utf8") as file:
     file.write(rendered_page)
+
 
 server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
